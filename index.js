b89fc6ee2466fb4b6e4deb4fa4c34f209fd71669
3mer7 <142077559+3mer7@users.noreply.github.com>
2024-05-20 21:47:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let links = document.getElementById("navlinks")
 let openn = document.getElementById("openbtn")
 let closee = document.getElementById("closefun")
+let upbtn = document.querySelector(".up")
 
 openn.onclick = () =>{
     links.style.right = "0"
@@ -8,4 +9,21 @@ openn.onclick = () =>{
 
 closee.onclick = () =>{
     links.style.right = "-1000px"
+}
+
+window.onscroll = function(){
+    if(window.scrollY >= 600){
+        upbtn.style.display = "block" 
+    }
+    else {
+        upbtn.style.display = "none" 
+    }
+}
+
+upbtn.onclick = () =>{
+    
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
 }
